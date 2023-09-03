413ffc6ec4dab466014e0974abbd890f3d0f1ffb
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-09-03 15:19:39 +0500

    Update __main__.py

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -1,30 +1,41 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 import os
-import subprocess
-import nltk
 import wikipedia
 
-nltk.download("punkt")  # Download NLTK data (if not already downloaded)
+# Create a chatbot instance
+chatbot = ChatBot('Felicia')
 
-from nltk.chat.util import Chat, reflections
+# Create a new trainer for the chatbot
+trainer = ChatterBotCorpusTrainer(chatbot)
+
+# Train the chatbot on English language data
+trainer.train('chatterbot.corpus.english')
 
 def main():
-    print("Hello! I'm Felicia, a female AI chatbot. How can I assist you today?")
+    print("Hello! I'm Felicia, your friendly chatbot. How can I assist you today?")
     
     while True:
         user_input = input("You >> ")
-        response = chat_with_felicia(user_input)
+        if user_input.lower() == "quit":
+            print("Felicia >> Goodbye! Have a great day!")
+            break
+        elif "search Wikipedia" in user_input:
+            topic = user_input.split("search Wikipedia")[1].strip()
+            response = get_wikipedia_summary(topic)
+        else:
+            response = chatbot.get_response(user_input)
+            
         print("Felicia >>", response)
+        
+        # Use Termux TTS to speak the response
         os.system(f"termux-tts-speak '{response}'")
 
-def chat_with_felicia(input_text):
-    if "connect to Wikipedia" in input_text:
-        topic = input_text.split("connect to Wikipedia")[1].strip()
-        try:
-            return wikipedia.summary(topic)
-        except wikipedia.exceptions.DisambiguationError as e:
-            return wikipedia.summary(e.options[0])
-        except wikipedia.exceptions.PageError:
-            return "No results found for that topic."
-    else:
-        return "I can assist you with a wide range of topics. Please specify your question."
-
+def get_wikipedia_summary(topic):
+    try:
+        summary = wikipedia.summary(topic)
+        return summary
+    except wikipedia.exceptions.DisambiguationError as e:
+        return "Wikipedia Disambiguation: " + str(e.options)
+    except wikipedia.exceptions.PageError:
+        return "No results found for that Wikipedia topic."

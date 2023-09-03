50c8b780c7c3fe3f75cf98855e8bfe2d907778e8
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-09-03 14:59:41 +0500

    Update __main__.py

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -1,25 +1,55 @@
 import os
 import subprocess
-from chatbot import Chat, register_call
-import wikipedia
+import nltk
+import wikipedia  # For connecting to Wikipedia
 
+nltk.download("punkt")  # Download NLTK data (if not already downloaded)
 
-@register_call("whoIs")
-def who_is(session, query):
-    try:
-        return wikipedia.summary(query)
-    except Exception:
-        for new_query in wikipedia.search(query):
-            try:
-                return wikipedia.summary(new_query)
-            except Exception:
-                pass
-    return "I don't know about "+query
+from nltk.chat.util import Chat, reflections
 
-def main():
-  while True:
-    inp = input("You >> ")
-    out = Chat("").converse(inp)
-    print("Felicia >>"+out)
-    fout = f"termux-tts-speak '{out}'"
-    os.system(fout)
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, how can I assist you today?",]
+    ],
+    [
+        r"what is your name?",
+        ["I'm Felicia, a female AI chatbot.",]
+    ],
+    [
+        r"how are you?",
+        ["I'm just a computer program, so I don't have feelings, but I'm here to help you.",]
+    ],
+    [
+        r"(.*) (weather|temperature) in (.*)",
+        ["I'm sorry, I cannot provide real-time weather information.",]
+    ],
+    [
+        r"(.*) help (.*)",
+        ["I can assist you with a wide range of topics. Please specify your question.",]
+    ],
+    [
+        r"(.*) your name?",
+        ["I told you, I'm Felicia, a female AI chatbot.",]
+    ],
+    [
+        r"(.*) (connect|search) to Wikipedia (.*)",
+        [wikipedia.summary("%3"),]
+    ],
+    [
+        r"quit",
+        ["Goodbye! If you have more questions, feel free to ask later.",]
+    ],
+]
+
+def felicia_chatbot():
+    print("Hello! I'm Felicia, a female AI chatbot. How can I assist you today?")
+    chat = Chat(pairs, reflections)
+    while True:
+        user_input = input("You >> ")
+        response = chat.respond(user_input)
+        print("Felicia >>", response)
+        os.system(f"termux-tts-speak '{response}'")
+
+if __name__ == "__main__":
+    felicia_chatbot()

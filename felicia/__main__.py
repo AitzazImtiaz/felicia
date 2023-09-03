74b97b2acf2b0df4b05aa6865397d9056c1b06b7
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-09-03 14:49:03 +0500

    Update __main__.py

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -1,11 +1,25 @@
-import cleverbotfreeapi
 import os
 import subprocess
+from chatbot import Chat, register_call
+import wikipedia
+
+
+@register_call("whoIs")
+def who_is(session, query):
+    try:
+        return wikipedia.summary(query)
+    except Exception:
+        for new_query in wikipedia.search(query):
+            try:
+                return wikipedia.summary(new_query)
+            except Exception:
+                pass
+    return "I don't know about "+query
 
 def main():
   while True:
     inp = input("You >> ")
-    out = cleverbotfreeapi.cleverbot(inp, session="Deftera")
+    out = Chat("examples/Example.template").converse(inp)
     print("Felicia >>"+out)
     fout = f"termux-tts-speak '{out}'"
     os.system(fout)

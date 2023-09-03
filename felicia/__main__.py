48689dd824046964efef41a0cbf841586a3615ee
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-09-03 13:32:43 +0500

    Update __main__.py

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -3,22 +3,10 @@ import os
 import subprocess
 
 def main():
-    while True:
-        try:
-            inp = subprocess.getoutput("termux-speech-to-text")
-            if not inp:
-                print("No input detected. Try again.")
-                continue
-            
-            print("You >> " + inp)
-            out = cleverbotfreeapi.cleverbot(inp, session="Deftera")
-            print("Felicia >> " + out)
-
-            # Use Termux's TTS to speak the response
-            os.system(f"termux-tts-speak '{out}'")
-        except Exception as e:
-            print("An error occurred:", str(e))
-            continue
-
-if __name__ == "__main__":
-    main()
+  while True:
+    inp = subprocess.getoutput("termux-speech-to-text")
+    print("You >> "+inp)
+    out = cleverbotfreeapi.cleverbot(inp, session="Deftera")
+    print("Felicia >>"+out)
+    fout = f"termux-tts-speak '{out}'"
+    os.system(fout)

7e208b0a19c6aeb823f4985a7374cad6cc3bd2be
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-09-03 08:42:06 +0500

    Update __main__.py

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -1,12 +1,27 @@
+import speech_recognition as sr
 import cleverbotfreeapi
 import os
-import subprocess
 
 def main():
-  while True:
-    inp = subprocess.getoutput("termux-speech-to-text")
-    print("You >> "+inp)
-    out = cleverbotfreeapi.cleverbot(inp, session="Deftera")
-    print("Felicia >>"+out)
-    fout = f"termux-tts-speak '{out}'"
-    os.system(fout)
+    recognizer = sr.Recognizer()
+    
+    while True:
+        with sr.Microphone() as source:
+            print("Say something:")
+            audio = recognizer.listen(source)
+        
+        try:
+            inp = recognizer.recognize_google(audio)
+            print("You >> " + inp)
+            
+            out = cleverbotfreeapi.cleverbot(inp, session="Deftera")
+            print("Felicia >> " + out)
+            
+            os.system(f"termux-tts-speak '{out}'")
+        except sr.UnknownValueError:
+            print("Could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+
+if __name__ == "__main__":
+    main()

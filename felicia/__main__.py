10d0dc7f189dd52251a4681786c0a57b3686b9f5
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-10-13 22:59:50 +0500

    Update __main__.py

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -1,26 +1,18 @@
-import os
 import cleverbotfreeapi
-
+import os
+import time
 def main():
     user_input = "Hi"  # Initial input from the user
     session = "Deftera"
-    
-    proxy_index = 0  # Start with the first proxy
-
     while True:
         print("You >> " + user_input)
-
-        # Use the current proxy for the request using proxychains
-        proxy_command = f"proxychains4 -q cleverbotfreeapi.cleverbot -s '{session}' '{user_input}'"
-        ai_response = os.popen(proxy_command).read()
+        ai_response = cleverbotfreeapi.cleverbot(user_input, session=session)
 
         if ai_response.strip():  # Check if Cleverbot provided a non-empty response
             print("AI 1 >> " + ai_response)
             os.system(f"espeak '{ai_response}'")  # Use espeak for AI 1's response
-            user_input = ai_response
+            user_input = cleverbotfreeapi.cleverbot(ai_response, session=session)
         else:
             print("AI 1 >> Cleverbot did not respond.")
             break
-
-        # Rotate to the next proxy or loop back to the first one
-        proxy_index = (proxy_index + 1) % len(proxies)
+        time.sleep(5)

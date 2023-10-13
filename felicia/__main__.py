eae87fa4d4eeafe3474f8c9601f7c56fec171e12
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-10-13 22:53:28 +0500

    Update __main__.py

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -1,20 +1,26 @@
-import gpt_2_simple as gpt2
 import os
+import cleverbotfreeapi
 
 def main():
-    sess = gpt2.start_tf_sess()
-    gpt2.load_gpt2(sess, model_name='124M')
-
     user_input = "Hi"  # Initial input from the user
+    session = "Deftera"
+    
+    proxy_index = 0  # Start with the first proxy
 
     while True:
         print("You >> " + user_input)
-        ai_response = gpt2.generate(sess, model_name='124M', prefix=user_input, return_as_list=True)[0]
 
-        if ai_response.strip():  # Check if the response is not empty
+        # Use the current proxy for the request using proxychains
+        proxy_command = f"proxychains4 -q cleverbotfreeapi.cleverbot -s '{session}' '{user_input}'"
+        ai_response = os.popen(proxy_command).read()
+
+        if ai_response.strip():  # Check if Cleverbot provided a non-empty response
             print("AI 1 >> " + ai_response)
             os.system(f"espeak '{ai_response}'")  # Use espeak for AI 1's response
             user_input = ai_response
         else:
-            print("AI 1 >> AI did not respond.")
+            print("AI 1 >> Cleverbot did not respond.")
             break
+
+        # Rotate to the next proxy or loop back to the first one
+        proxy_index = (proxy_index + 1) % len(proxies)

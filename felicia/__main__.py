57fe2d632d43a95552c110c6e71a8cb2db0b3a3d
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-10-13 23:04:50 +0500

    Update __main__.py

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -1,18 +1,25 @@
 import cleverbotfreeapi
 import os
 import time
+
 def main():
     user_input = "Hi"  # Initial input from the user
     session = "Deftera"
+    
     while True:
         print("You >> " + user_input)
+        user_input_cleaned = user_input.replace("'", '')  # Remove single quotes
+        os.system(f"termux-tts-speak '{user_input_cleaned}'")
+        
         ai_response = cleverbotfreeapi.cleverbot(user_input, session=session)
 
         if ai_response.strip():  # Check if Cleverbot provided a non-empty response
-            print("AI 1 >> " + ai_response)
-            os.system(f"espeak '{ai_response}'")  # Use espeak for AI 1's response
-            user_input = cleverbotfreeapi.cleverbot(ai_response, session=session)
+            ai_response_cleaned = ai_response.replace("'", '')  # Remove single quotes
+            print("AI 1 >> " + ai_response_cleaned)
+            os.system(f"espeak '{ai_response_cleaned}'")  # Use espeak for AI 1's response
+            user_input = ai_response
         else:
             print("AI 1 >> Cleverbot did not respond.")
             break
+
         time.sleep(5)

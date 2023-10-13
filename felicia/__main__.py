5dbb9eb6e14f91c35c6f65cb0f794d0e4cd2027f
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-10-13 22:21:51 +0500

    Update __main__.py#

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -1,41 +1,22 @@
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+import cleverbotfreeapi
 import os
-import wikipedia
-
-# Create a chatbot instance
-chatbot = ChatBot('Felicia')
-
-# Create a new trainer for the chatbot
-trainer = ChatterBotCorpusTrainer(chatbot)
-
-# Train the chatbot on English language data
-trainer.train('chatterbot.corpus.english')
+import subprocess
 
 def main():
-    print("Hello! I'm Felicia, your friendly chatbot. How can I assist you today?")
+    first_inp = "Hi"  # Initial input
+    session = "Deftera"
     
     while True:
-        user_input = input("You >> ")
-        if user_input.lower() == "quit":
-            print("Felicia >> Goodbye! Have a great day!")
-            break
-        elif "search Wikipedia" in user_input:
-            topic = user_input.split("search Wikipedia")[1].strip()
-            response = get_wikipedia_summary(topic)
-        else:
-            response = chatbot.get_response(user_input)
-            
-        print("Felicia >>", response)
+        print("You >> " + first_inp)
+        out = cleverbotfreeapi.cleverbot(first_inp, session=session)
+        print("AI 1 >> " + out)
         
-        # Use Termux TTS to speak the response
-        os.system(f"termux-tts-speak '{response}'")
-
-def get_wikipedia_summary(topic):
-    try:
-        summary = wikipedia.summary(topic)
-        return summary
-    except wikipedia.exceptions.DisambiguationError as e:
-        return "Wikipedia Disambiguation: " + str(e.options)
-    except wikipedia.exceptions.PageError:
-        return "No results found for that Wikipedia topic."
+        if out.strip():  # Check if Cleverbot provided a non-empty response
+            second_inp = out
+            print("AI 2 >> " + second_inp)
+            fout = f"termux-tts-speak '{second_inp}'"
+            os.system(fout)
+            first_inp = second_inp  # Update the input for the next interaction
+        else:
+            print("AI 2 >> Cleverbot did not respond.")
+            break

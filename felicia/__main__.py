32d1682ed175c664cccc632097f08b4ce348ccf9
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-10-13 23:23:07 +0500

    Update __main__.py

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -10,7 +10,7 @@ def main():
         print("You >> " + user_input)
         os.system(f"termux-tts-speak '{ai_response}'")  # Use espeak for AI 1's response
         ai_response = cleverbotfreeapi.cleverbot(user_input, session=session)
-
+        time.sleep(2.5)
         if ai_response.strip():  # Check if Cleverbot provided a non-empty response
             print("AI 1 >> " + ai_response)
             os.system(f"espeak '{ai_response}'")  # Use espeak for AI 1's response
@@ -18,3 +18,4 @@ def main():
         else:
             print("AI 1 >> Cleverbot did not respond.")
             break
+        time.sleep(2.5)

81e1368891869d8b620afcbbd55f9f5b78e971fc
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-10-13 22:37:46 +0500

    Update __main__.py

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -1,18 +1,20 @@
-import cleverbotfreeapi
+import gpt_2_simple as gpt2
 import os
 
 def main():
+    sess = gpt2.start_tf_sess()
+    gpt2.load_gpt2(sess, model_name='124M')
+
     user_input = "Hi"  # Initial input from the user
-    session = "Deftera"
 
     while True:
         print("You >> " + user_input)
-        ai_response = cleverbotfreeapi.cleverbot(user_input, session=session)
+        ai_response = gpt2.generate(sess, model_name='124M', prefix=user_input, return_as_list=True)[0]
 
-        if ai_response.strip():  # Check if Cleverbot provided a non-empty response
+        if ai_response.strip():  # Check if the response is not empty
             print("AI 1 >> " + ai_response)
             os.system(f"espeak '{ai_response}'")  # Use espeak for AI 1's response
-            user_input = cleverbotfreeapi.cleverbot(ai_response, session=session)
+            user_input = ai_response
         else:
-            print("AI 1 >> Cleverbot did not respond.")
+            print("AI 1 >> AI did not respond.")
             break

3dd2a0475ea50b9300c62a81e1e58011a09be3fc
Aitzaz Imtiaz <aitzazimtiaz855@gmail.com>
2023-10-13 22:28:20 +0500

    Update __main__.py

--- felicia/__main__.py
+++ felicia/__main__.py
@@ -1,21 +1,20 @@
 import cleverbotfreeapi
 import os
-import subprocess
 
 def main():
     user_input = "Hi"  # Initial input from the user
     session = "Deftera"
-    
+
     while True:
         print("You >> " + user_input)
         ai_response = cleverbotfreeapi.cleverbot(user_input, session=session)
-        
+
         if ai_response.strip():  # Check if Cleverbot provided a non-empty response
             print("AI 1 >> " + ai_response)
-            user_input = ai_response  # User's response is based on AI 1's reply
+            user_input = cleverbotfreeapi.cleverbot(ai_response, session=session)
+            print("You >> " + user_input)
+            fout = f"termux-tts-speak '{user_input}'"
+            os.system(fout)
         else:
             print("AI 1 >> Cleverbot did not respond.")
             break
-
-if __name__ == "__main__":
-    main()
